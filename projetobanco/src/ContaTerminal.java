1215695a3496e2762d7f6420ddd2ea8a3f423ba2
qtm08 <queilateixeira03@yahoo.com>
2024-06-05 20:38:35 -0300

    j

--- projetobanco/src/ContaTerminal.java
+++ projetobanco/src/ContaTerminal.java
@@ -1,31 +1,42 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author USUARIO
+ */
 public class ContaTerminal {
+
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-    Scanner scanner = new Scanner (System.in);
-        
-    System.out.println("Por favor, digite o número da Conta: ");
-        int numero = scanner.nextInt();
-        scanner.nextLine();
-        
-    System.out.println("Por favor, digite o número da Agência: ");
-        string agencia = scanner.nextLine();
-        scanner.nextLine();
-        
-    System.out.println("Por favor, digite seu nome: ");
-        string nomeCliente = scanner.nextLine();
-
-    System.out.println("Por favor, digite o Saldo !");
-        double saldo = scanner.nextDouble();
-        scanner.nextLine();
-
-    String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
-                .concat(agencia)
-                .concat(", conta ")
-                .concat(Integer.toString(numero))
-                .concat(" e seu saldo ")
-                .concat(Double.toString(saldo))
-                .concat(" já está disponível para saque.");
-        System.out.println(mensagem);
-        
-    scanner.close();
+        try (Scanner scanner = new Scanner (System.in)) {
+            System.out.println("Por favor, digite o número da Conta: ");
+            int numero = scanner.nextInt();
+            scanner.nextLine();
+            
+            System.out.println("Por favor, digite o número da Agência: ");
+            String agencia = scanner.nextLine();
+            scanner.nextLine();
+            
+            System.out.println("Por favor, digite seu nome: ");
+            String nomeCliente = scanner.nextLine();
+            
+            System.out.println("Por favor, digite o Saldo !");
+            double saldo = scanner.nextDouble();
+            scanner.nextLine();
+            
+            String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                    .concat(agencia)
+                    .concat(", conta ")
+                    .concat(Integer.toString(numero))
+                    .concat(" e seu saldo ")
+                    .concat(Double.toString(saldo))
+                    .concat(" já está disponível para saque.");
+            System.out.println(mensagem);
+        }
     }
+
 }
